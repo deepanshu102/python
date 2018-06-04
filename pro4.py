086158a17a6a3ccf0c31da9b05e25e5a94bcbb86
deepanshu102 <26045021+deepanshu102@users.noreply.github.com>
2018-06-05 00:28:43 +0530

    Exercise1
    1 exercise of python

---
+++ pro4.py
@@ -0,0 +1,10 @@
+##################
+##  problem 4  ##
+#################
+mylist=[1,1,1,1,1,2,2,2,2,3,3,3,3]
+a=int(input('enter one interger value'))
+if(a in mylist):
+    print("a is exist in the given list");
+else:
+    print("a is not exist in the given list");
+
---
+++ pro5.py
@@ -0,0 +1,6 @@
+#################
+###problem 5####
+###############
+age=4
+name="Sammy"
+print("Hello my dog's name is "+name+" and he is "+str(age)+" years old");
